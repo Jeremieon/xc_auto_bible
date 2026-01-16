b90f9ae37098d35e618166b875d33f8a3962055e
Jeremieon <jerry_slade@outlook.com>
2026-01-16 22:10:46 +0100

    done_dear1

--- customer-edge-lb/f5ce-aws/output.tf
+++ customer-edge-lb/f5ce-aws/output.tf
@@ -6,9 +6,9 @@ output "vpc_id" {
 }
 
 output "private_app_ip" {
-  value = aws_instance.private_app_server.private_ip
+  value = aws_instance.private_app_server[0].private_ip
 }
 
 output "ce_public_ip" {
-  value = aws_eip.public_ip.private_ip.public_ip
+  value = aws_eip.public_ip
 }
