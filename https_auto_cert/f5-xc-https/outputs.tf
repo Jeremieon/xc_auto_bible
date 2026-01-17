3984ddd75d17dc1487ed027916e80e679a635c09
Jeremieon <jerry_slade@outlook.com>
2026-01-17 21:44:00 +0100

    convert

--- https_auto_cert/f5-xc-https/outputs.tf
+++ https_auto_cert/f5-xc-https/outputs.tf
@@ -3,8 +3,9 @@ output "http_lb_fqdn" {
 }
 
 output "http_lb_public_ip" {
-  value = data.volterra_http_loadbalancer_state.lb-state.ip_address
+  value = volterra_http_loadbalancer.https_auto_cert-lb.domains
 }
+
 
 output "http_lb_state" {
   value = data.volterra_http_loadbalancer_state.lb-state.state
