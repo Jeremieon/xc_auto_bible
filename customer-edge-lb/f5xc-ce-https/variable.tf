f5a506af9ed8c6e6222e2c91d796eaec87165925
Jeremieon <jerry_slade@outlook.com>
2026-01-18 00:00:57 +0100

    fixed error

--- customer-edge-lb/f5xc-ce-https/variable.tf
+++ customer-edge-lb/f5xc-ce-https/variable.tf
@@ -19,11 +19,13 @@ variable "domain" {
 variable "inside_network_IP" {
   description = "Inside network IP where the backend server is located for origin pool"
   type        = string
+  default     = "0.0.0.0"
 }
 
 variable "site_name" {
   description = "F5XC site name where the backend server is located"
   type        = string
+  default     = "jeremieon-site-xyz"
 }
 
 variable "site_adv_network" {
