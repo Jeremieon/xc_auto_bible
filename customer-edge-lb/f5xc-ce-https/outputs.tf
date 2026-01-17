7e6efd1ab8418d2abd58d303763647273a861acf
Jeremieon <jerry_slade@outlook.com>
2026-01-17 21:36:13 +0100

    convert

--- customer-edge-lb/f5xc-ce-https/outputs.tf
+++ customer-edge-lb/f5xc-ce-https/outputs.tf
@@ -3,11 +3,12 @@ output "http_lb_fqdn" {
 }
 
 output "http_lb_public_ip" {
-  value = volterra_http_loadbalancer_state.https_lb.ip_address
+  value = volterra_http_loadbalancer.https_lb.ip_address
 }
 
 output "http_lb_state" {
   value = data.volterra_http_loadbalancer_state.lb-state.state
 }
+
 
 
