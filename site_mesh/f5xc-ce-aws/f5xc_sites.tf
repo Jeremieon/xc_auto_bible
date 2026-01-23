5ca17c6fe8614bff10f1d04a29a3a97b42655c3f
Jeremieon <jerry_slade@outlook.com>
2026-01-23 15:42:04 +0100

    fixed Bugs

--- site_mesh/f5xc-ce-aws/f5xc_sites.tf
+++ site_mesh/f5xc-ce-aws/f5xc_sites.tf
@@ -36,8 +36,25 @@ resource "volterra_securemesh_site_v2" "site" {
   aws {
     not_managed {
       node_list {
+        hostname  = "master-0"
         type      = "Control"
         public_ip = aws_eip.public_ip.public_ip
+        #OUTSIDE INTERFACE
+        interface_list {
+          ethernet_interface {
+            device = "eth0"
+          }
+          static_ip {
+            ip_address = var.outside_subnet_cidr
+          }
+          name = "eth0"
+          network_option {
+            site_local_network = true
+          }
+
+          site_to_site_connectivity_interface_enabled = true
+        }
+        #INSIDE INTERFACE
         interface_list {
           ethernet_interface {
             device = "eth1"
@@ -45,11 +62,14 @@ resource "volterra_securemesh_site_v2" "site" {
           static_ip {
             ip_address = var.inside_subnet_cidr
           }
+          name = "eth1"
           network_option {
-            site_local_network = true
+            site_local_inside_network = true
           }
-          site_to_site_connectivity_interface_enabled = true
+
+          site_to_site_connectivity_interface_enabled = false
         }
+
 
       }
 
