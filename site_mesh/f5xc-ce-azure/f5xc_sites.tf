58eb4678645b83989400340c81649295f67b8eb4
Jeremieon <jerry_slade@outlook.com>
2026-01-23 12:51:51 +0100

    fixed Bugs

--- site_mesh/f5xc-ce-azure/f5xc_sites.tf
+++ site_mesh/f5xc-ce-azure/f5xc_sites.tf
@@ -34,6 +34,8 @@ resource "volterra_securemesh_site_v2" "site" {
   azure {
     not_managed {
       node_list {
+        hostname  = Control
+        type      = "Control"
         public_ip = azurerm_public_ip.ce_public_ip
         interface_list {
           bond_interface {
